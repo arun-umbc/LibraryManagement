56242be3b38f17232a83321c101c4396fdf3b204
arun-umbc <arunk1@umbc.edu>
2022-05-02 15:40:40 -0400

    fixes

--- library_management/library_management/settings.py
+++ library_management/library_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&3$#f+%*=f_bo@z4$dm&+9@o*oyfuj(i)1p(0fnc5+h!*u7e@5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
