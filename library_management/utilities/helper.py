ab919e3556f771718cc3f0e9cce94843d12d2674
arun-umbc <arunk1@umbc.edu>
2022-05-03 19:06:51 -0400

    test DP

--- library_management/utilities/helper.py
+++ library_management/utilities/helper.py
@@ -1,5 +1,11 @@
+import os
 import time
 from hashlib import md5
+
+import pandas as pd
+from pydp.algorithms.laplacian import Count
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 def create_response_dict(data, message, success=True, page=None, **kwargs):
@@ -16,3 +22,19 @@ def create_response_dict(data, message, success=True, page=None, **kwargs):
 
 def create_db_id():
     return md5(f"{time.time()}".encode('utf-8')).hexdigest()
+
+
+def test_dp(epsilon):
+    try:
+        grouped_data = pd.read_csv(BASE_DIR + "/data/group_by_dept.csv")
+        read_count = list(grouped_data['read_count'])
+        dp_read_count = list()
+
+        for data in read_count:
+            x = Count(epsilon)
+            count = x.quick_result(list(range(1, data + 1)))
+            dp_read_count.append(count)
+        grouped_data['dp_read_count'] = dp_read_count
+        grouped_data.to_csv(BASE_DIR + "/data/group_by_dept_dp.csv")
+    except Exception as e:
+        print(e)
